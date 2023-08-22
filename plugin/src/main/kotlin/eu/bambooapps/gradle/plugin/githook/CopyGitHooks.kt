6630c8eae0fe37cf27d0db75ed48a8ea0d73dcd0
Andrei Mukamolau <amv@bamboogroup.eu>
2023-08-22 16:02:05 +0300

    Added custom task

---
+++ plugin/src/main/kotlin/eu/bambooapps/gradle/plugin/githook/CopyGitHooks.kt
@@ -0,0 +1,40 @@
+package eu.bambooapps.gradle.plugin.githook
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import javax.inject.Inject
+
+abstract class CopyGitHooks : DefaultTask() {
+
+    @get:InputDirectory
+    abstract val gitHooksDirectory: DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val gitHooksDestinationDirectory: DirectoryProperty
+
+    @get:Inject
+    abstract val fileSystemOperations: FileSystemOperations
+
+    @TaskAction
+    fun copyGitHooks() {
+        fileSystemOperations.copy {
+            from(gitHooksDirectory) {
+                include("**/*.sh")
+                rename("(.*).sh", "$1")
+            }
+            into(gitHooksDestinationDirectory)
+            onlyIf { isLinuxOrMacOs() }
+        }
+    }
+
+    private fun isLinuxOrMacOs(): Boolean {
+        val osName = System.getProperty("os.name").lowercase()
+        return osName.contains("linux") ||
+                osName.contains("mac os") ||
+                osName.contains("macos")
+    }
+}
